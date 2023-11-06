6adcc166fb1e4a8a0b1e98edbdfa5ccb4bf3be07
Alicja Mazur <mazur.alicja4@gmail.com>
2023-11-06 21:35:39 +0100

    change var name and matrix copy order

--- part3/alicja/heat-mpi.c
+++ part3/alicja/heat-mpi.c
@@ -32,7 +32,7 @@ int main( int argc, char *argv[] )
 
     // algorithmic parameters
     algoparam_t param;
-    int np;
+    int np, rows_per_proc;
 
     double runtime, flop;
     double residual=0.0;
@@ -97,8 +97,8 @@ int main( int argc, char *argv[] )
     // full size (param.resolution are only the inner points)
     np = param.resolution + 2;
 
-    // TODO compute number of rows per worker
-    int rows = param.resolution/numprocs;
+    // TODO compute number of rows per process
+    rows_per_proc = param.resolution / numprocs;
 
     // starting time
     runtime = wtime();
@@ -110,8 +110,8 @@ int main( int argc, char *argv[] )
                 MPI_Send(&param.resolution, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
                 MPI_Send(&param.algorithm, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
                 // TODO send distinct segment (with ghost points) to each worker
-                MPI_Send(&param.u[i*(rows*np)], (rows+2)*(np), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-                MPI_Send(&param.uhelp[i*(rows*np)], (rows+2)*(np), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+                MPI_Send(&param.u[i*(rows_per_proc*np)], (rows_per_proc+2)*np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+                MPI_Send(&param.uhelp[i*(rows_per_proc*np)], (rows_per_proc+2)*np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
         }
     }
 
@@ -120,32 +120,33 @@ int main( int argc, char *argv[] )
 	switch( param.algorithm ) {
 	    case 0: // JACOBI
 	            // TODO compute values for internal points of the first segment of the matrix
-	            residual = relax_jacobi(param.u, param.uhelp, rows+2, np);
+	            residual = relax_jacobi(param.u, param.uhelp, rows_per_proc+2, np);
 
-            // TODO send the last row to the next process (after the first iteration of computing values, because ghost points haven't changed yet)
-            MPI_Send(&param.uhelp[rows*np], np, MPI_DOUBLE, myid + 1, 0, MPI_COMM_WORLD);
-
-		    // Copy uhelp into u
+            // Copy uhelp into u
 		    // TODO update values of the first segment (with ghost points - what for, if they were only read?) of the matrix
-		    for (int i=0; i<(rows+2); i++)
+		    for (int i=0; i<(rows_per_proc+2); i++)
     		        for (int j=0; j<np; j++)
 	    		    param.u[ i*np+j ] = param.uhelp[ i*np+j ];
 
+            // TODO send the last row to the next process (after the first iteration of computing values, because ghost points haven't changed yet)
+            MPI_Send(&param.u[rows_per_proc*np], np, MPI_DOUBLE, myid+1, iter, MPI_COMM_WORLD);
+
 	        // TODO receive the first row from the next process and save it in the last row (to update ghost points for the next iteration)
-            MPI_Recv(&param.u[(rows*np)+np], np, MPI_DOUBLE, myid + 1, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&param.u[(rows_per_proc*np)+np], np, MPI_DOUBLE, myid+1, iter, MPI_COMM_WORLD, &status);
 
 		    break;
 	    case 1: // RED-BLACK
 		    residual = relax_redblack(param.u, np, np);
 		    break;
 	    case 2: // GAUSS
-		    residual = relax_gauss(param.u, np, np);
+	        // TODO compute values for internal points of the first segment of the matrix
+	        residual = relax_gauss(param.u, rows_per_proc+2, np);
 		    break;
 	    }
 
         iter++;
 
-        // TODO get the current value of global residual
+        // TODO compute the current value of global residual
         MPI_Allreduce(&residual, &gresidual, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
         // solution good enough ?
@@ -157,7 +158,7 @@ int main( int argc, char *argv[] )
 
     // TODO receive final values of internal points of each segment from workers
     for (int i=1; i<numprocs; i++)
-        MPI_Irecv(&param.u[i*(rows*np)+np], rows*np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &request[i-1]);
+        MPI_Irecv(&param.u[(rows_per_proc*i+1)*np], rows_per_proc*np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &request[i-1]);
 
     // TODO wait until all workers send final values for their segments
     MPI_Waitall(numprocs-1, request, MPI_STATUS_IGNORE);
@@ -195,7 +196,7 @@ int main( int argc, char *argv[] )
 
     // receive information from master to perform computation locally
 
-    int columns, rows, np;
+    int columns, rows_per_proc, np;
     int iter, maxiter;
     int algorithm;
     double residual;
@@ -207,53 +208,53 @@ int main( int argc, char *argv[] )
     MPI_Recv(&algorithm, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
     // TODO define number of rows in a segment computed by each worker
-    rows = columns / numprocs;
+    rows_per_proc = columns / numprocs;
     np = columns + 2;
 
     // TODO reduce memory allocated to each worker to fit only one segment with ghost points
     // allocate memory for worker
-    double * u = calloc( sizeof(double),(rows+2)*(columns+2) );
-    double * uhelp = calloc( sizeof(double),(rows+2)*(columns+2) );
+    double * u = calloc( sizeof(double),(rows_per_proc+2)*(columns+2) );
+    double * uhelp = calloc( sizeof(double),(rows_per_proc+2)*(columns+2) );
     if( (!u) || (!uhelp) )
     {
         fprintf(stderr, "Error: Cannot allocate memory\n");
         return 0;
     }
-    
+
     // fill initial values for matrix with values received from master
     // TODO receive a single distinct segment from master (with ghost points)
-    MPI_Recv(&u[0], (rows+2)*(columns+2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
-    MPI_Recv(&uhelp[0], (rows+2)*(columns+2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(&u[0], (rows_per_proc+2)*(columns+2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(&uhelp[0], (rows_per_proc+2)*(columns+2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
 
     iter = 0;
     while(1) {
 	switch( algorithm ) {
 	    case 0: // JACOBI
 	            // TODO compute values for assigned segment of the matrix
-	            residual = relax_jacobi(u, uhelp, rows+2, np);
+	            residual = relax_jacobi(u, uhelp, rows_per_proc+2, np);
 
-	        //  TODO send the last row (of internal points) to the next process, except the last process
-	        if (myid < numprocs-1)
-            MPI_Isend(&uhelp[rows*np], np, MPI_DOUBLE, myid + 1, 0, MPI_COMM_WORLD, &r[2]);
-
-            // TODO send the first row (of internal points) to the previous process
-            MPI_Isend(&uhelp[np], np, MPI_DOUBLE, myid - 1, 0, MPI_COMM_WORLD, &r[0]);
-
-		    // Copy uhelp into u
+            // Copy uhelp into u
 		    // TODO update values of computed segment of the matrix
-		    for (int i=0; i<(rows+2); i++)
+		    for (int i=0; i<(rows_per_proc+2); i++)
     		        for (int j=0; j<np; j++)
 	    		    u[ i*np+j ] = uhelp[ i*np+j ];
 
-		    // TODO receive the last row from the previous process and store it in the first row (after the first iteration of computing values, because ghost points haven't changed yet)
-		    MPI_Irecv(&u[0], np, MPI_DOUBLE, myid - 1, 0, MPI_COMM_WORLD, &r[1]);
+	        //  TODO send the last row (of internal points) to the next process, except the last process
+	        if (myid < numprocs-1)
+            MPI_Isend(&u[rows_per_proc*np], np, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD, &r[2]);
+
+            // TODO send the first row (of internal points) to the previous process
+            MPI_Isend(&u[np], np, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD, &r[0]);
+
+		    // TODO receive the last row from the previous process and store it in the first row as ghost points (after the first iteration of computing values, because ghost points haven't changed yet)
+		    MPI_Irecv(&u[0], np, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD, &r[1]);
 
             if (myid < numprocs-1)
-            // TODO receive the first row from the next process and store it in the last row
-            MPI_Irecv(&u[(rows*np)+np], np, MPI_DOUBLE, myid + 1, 0, MPI_COMM_WORLD, &r[3]);
+            // TODO receive the first row from the next process and store it in the last row as ghost points
+            MPI_Irecv(&u[(rows_per_proc*np)+np], np, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD, &r[3]);
 
             // TODO wait until the exchange of the first row is complete (r[0] and r[1])
-            MPI_Waitall(2, r, MPI_STATUS_IGNORE);
+            MPI_Waitall(2, r, MPI_STATUS_IGNORE); // TODO does it makes sense to use non-blocking here?
 
             // TODO wait until the exchange of the last row is complete (r[2] and r[3])
             if (myid < numprocs-1)
@@ -264,7 +265,8 @@ int main( int argc, char *argv[] )
 		    residual = relax_redblack(u, np, np);
 		    break;
 	    case 2: // GAUSS
-		    residual = relax_gauss(u, np, np);
+	        // TODO compute values for assigned segment of the matrix
+		    residual = relax_gauss(u, rows_per_proc+2, np);
 		    break;
 	    }
 
@@ -279,8 +281,8 @@ int main( int argc, char *argv[] )
         // max. iteration reached ? (no limit with maxiter=0)
         if (maxiter>0 && iter>=maxiter) break;
     }
-        // TODO send final values of internal points to master
-        MPI_Send(&u[np], rows*np, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    // TODO send final values of internal points to master
+    MPI_Send(&u[np], rows_per_proc*np, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 
     if( u ) free(u); 
     if( uhelp ) free(uhelp);
